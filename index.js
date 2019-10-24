9951206a5326f25f90512e7e7082eeb1feda7fc7
erv <ervinmitra@gmail.com>
2019-10-24 10:45:09 -0700

    took off api keys

--- index.js
+++ index.js
@@ -9,20 +9,8 @@ require('dotenv').config({ path: '.env' });
  * 
  */
 
-// works
-const url = `https://api.sportradar.us/nfl-t1/2019/PRE/0/den/atl/summary.json?api_key=tk5fsac8894yezpymuqw36x5`;
-
-// works, gets a season's schedule
-const url2 = `https://api.sportradar.us/nfl-t1/2018/REG/schedule.json?api_key=tk5fsac8894yezpymuqw36x5`;
-
-// DOESNT WORK
-const url3 = `https://api.sportradar.us/nfl-t1/030d37cf-b896-4f10-b16e-2a5120fef6cf/boxscore.json?api_key=tk5fsac8894yezpymuqw36x5`;
-
-// works
-const url4 = `https://api.sportradar.us/nfl-t1/2019/PRE/schedule.json?api_key=tk5fsac8894yezpymuqw36x5`;
-
-// works
-const url5 = `https://api.sportradar.us/nfl-t1/2019/REG/schedule.json?api_key=tk5fsac8894yezpymuqw36x5`;
+const season = `2019REG`;
+const url = `https://api.sportsdata.io/v3/nfl/scores/json/Standings/2019REG?key=${SPORTSDATAIO_API_KEY}`;
 const callAPI = async () => {
     try {
         const catchCall = await axios.get(url5);
